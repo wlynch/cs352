f4fca2e1d36e351cca852c6b67a6a240755a7c8d
Billy Lynch <wlynch92@gmail.com>
2013-02-09 16:24:50 -0500

    implemented CalcStack

--- assignment2/CalcStack.java
+++ assignment2/CalcStack.java
@@ -11,6 +11,104 @@ import java.util.*;
 
 public class CalcStack {
 
-    private Stack<Integer> stack;
+    private Stack<Double> stack;
 
+    /**
+     * Initializes new CalcStack.
+     */
+    public CalcStack() {
+        stack = new Stack<Double>();
+    }
+    
+    /**
+     * Runs the command that the server wishes to execute. 
+     * @param cmd Command to run.
+     * @return Null if number is pushed to the stack, 
+     * else a double[] containing the command results
+     * @throws EmptyStackException Thrown if the number of elements in 
+     * the stack is < 2
+     * @throws UnsupportedOperationException Thrown if the given command 
+     * is not valid
+     */
+    public double[] exec(String cmd) 
+    		throws EmptyStackException,UnsupportedOperationException{
+    	/*
+    	 * Try to parse the command to see if it is a number.
+    	 * If not, treat it as an operation.
+    	 */
+    	try {
+    		double d = Double.parseDouble(cmd);
+    		stack.push(d);
+    		return null;
+    	} catch (NumberFormatException e) {
+    		return operate(cmd);
+    	}
+    }
+    
+    /**
+     * Perform the given operation (+,-,*,/) on the stack
+     * @param cmd Operation to be performed
+     * @return The result of the operation performed
+     * @throws EmptyStackException Thrown if the size of the stack is < 2
+     * @throws UnsupportedOperationException Thrown if the given command 
+     * is not valid
+     */
+    public double[] operate(String cmd) 
+    		throws EmptyStackException,UnsupportedOperationException {
+    	double a,b,c;
+    	
+       	if (cmd.equals("show")) {
+    		return show();
+    	} else if (cmd.length() != 1){
+    		throw(new UnsupportedOperationException());
+    	}
+    	char op = cmd.charAt(0);
+    	
+    	a = stack.pop();
+    	
+    	/* If there is only 1 element in the stack, then we must push
+    	 * a back on the stack before throwing the exception 
+    	 */
+    	try {
+        	b = stack.pop();
+        } catch (EmptyStackException e) {
+        	stack.push(a);
+        	throw(new EmptyStackException());
+        }
+    	
+    	switch(op) {
+    	case '+':
+    		c = b+a;
+    		break;
+    	case '-':
+    		c = b-a;
+    		break;
+    	case '*':
+    		c = b*a;
+    		break;
+    	case '/':
+    		c = b/a;
+    		break;
+    	default:
+    		c=0;
+    		stack.push(b);
+    		stack.push(a);
+    		throw(new UnsupportedOperationException());
+    	}
+    	
+    	double[] retval = { stack.push(c) };
+    	return retval;    	
+    }
+    
+    public double[] show() {
+        double[] retval = new double[stack.size()];
+        int size = stack.size();
+        for(int i = 0; i < size; i++) {
+        	retval[i] = stack.pop();
+        }
+        for (int i = retval.length-1; i >= 0; i--) {
+        	stack.push(retval[i]);
+        }
+        return retval;
+    }  
 }
