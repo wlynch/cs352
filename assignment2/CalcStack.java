27671a10efd5be27abde50b499287590ebd7dd18
Bilal Quadri <bilalquadri92@gmail.com>
2013-02-09 18:35:04 -0500

    Fixed indentation problems and trailing whitespace

--- assignment2/CalcStack.java
+++ assignment2/CalcStack.java
@@ -19,96 +19,96 @@ public class CalcStack {
     public CalcStack() {
         stack = new Stack<Double>();
     }
-    
+
     /**
-     * Runs the command that the server wishes to execute. 
+     * Runs the command that the server wishes to execute.
      * @param cmd Command to run.
-     * @return Null if number is pushed to the stack, 
+     * @return Null if number is pushed to the stack,
      * else a double[] containing the command results
-     * @throws EmptyStackException Thrown if the number of elements in 
+     * @throws EmptyStackException Thrown if the number of elements in
      * the stack is < 2
-     * @throws UnsupportedOperationException Thrown if the given command 
+     * @throws UnsupportedOperationException Thrown if the given command
      * is not valid
      */
-    public double[] exec(String cmd) 
-    		throws EmptyStackException,UnsupportedOperationException{
-    	/*
-    	 * Try to parse the command to see if it is a number.
-    	 * If not, treat it as an operation.
-    	 */
-    	try {
-    		double d = Double.parseDouble(cmd);
-    		stack.push(d);
-    		return null;
-    	} catch (NumberFormatException e) {
-    		return operate(cmd);
-    	}
+    public double[] exec(String cmd)
+        throws EmptyStackException,UnsupportedOperationException{
+        /*
+         * Try to parse the command to see if it is a number.
+         * If not, treat it as an operation.
+         */
+        try {
+            double d = Double.parseDouble(cmd);
+            stack.push(d);
+            return null;
+        } catch (NumberFormatException e) {
+            return operate(cmd);
+        }
     }
-    
+
     /**
      * Perform the given operation (+,-,*,/) on the stack
      * @param cmd Operation to be performed
      * @return The result of the operation performed
      * @throws EmptyStackException Thrown if the size of the stack is < 2
-     * @throws UnsupportedOperationException Thrown if the given command 
+     * @throws UnsupportedOperationException Thrown if the given command
      * is not valid
      */
-    public double[] operate(String cmd) 
-    		throws EmptyStackException,UnsupportedOperationException {
-    	double a,b,c;
-    	
-       	if (cmd.equals("show")) {
-    		return show();
-    	} else if (cmd.length() != 1){
-    		throw(new UnsupportedOperationException());
-    	}
-    	char op = cmd.charAt(0);
-    	
-    	a = stack.pop();
-    	
-    	/* If there is only 1 element in the stack, then we must push
-    	 * a back on the stack before throwing the exception 
-    	 */
-    	try {
-        	b = stack.pop();
-        } catch (EmptyStackException e) {
-        	stack.push(a);
-        	throw(new EmptyStackException());
+    public double[] operate(String cmd)
+        throws EmptyStackException,UnsupportedOperationException {
+        double a,b,c;
+
+        if (cmd.equals("show")) {
+            return show();
+        } else if (cmd.length() != 1){
+            throw(new UnsupportedOperationException());
         }
-    	
-    	switch(op) {
-    	case '+':
-    		c = b+a;
-    		break;
-    	case '-':
-    		c = b-a;
-    		break;
-    	case '*':
-    		c = b*a;
-    		break;
-    	case '/':
-    		c = b/a;
-    		break;
-    	default:
-    		c=0;
-    		stack.push(b);
-    		stack.push(a);
-    		throw(new UnsupportedOperationException());
-    	}
-    	
-    	double[] retval = { stack.push(c) };
-    	return retval;    	
+        char op = cmd.charAt(0);
+
+        a = stack.pop();
+
+        /* If there is only 1 element in the stack, then we must push
+         * a back on the stack before throwing the exception
+         */
+        try {
+            b = stack.pop();
+        } catch (EmptyStackException e) {
+            stack.push(a);
+            throw(new EmptyStackException());
+        }
+
+        switch(op) {
+            case '+':
+                c = b+a;
+                break;
+            case '-':
+                c = b-a;
+                break;
+            case '*':
+                c = b*a;
+                break;
+            case '/':
+                c = b/a;
+                break;
+            default:
+                c=0;
+                stack.push(b);
+                stack.push(a);
+                throw(new UnsupportedOperationException());
+        }
+
+        double[] retval = { stack.push(c) };
+        return retval;
     }
-    
+
     public double[] show() {
         double[] retval = new double[stack.size()];
         int size = stack.size();
         for(int i = 0; i < size; i++) {
-        	retval[i] = stack.pop();
+            retval[i] = stack.pop();
         }
         for (int i = retval.length-1; i >= 0; i--) {
-        	stack.push(retval[i]);
+            stack.push(retval[i]);
         }
         return retval;
-    }  
+    }
 }
--- assignment2/StackTest.java
+++ assignment2/StackTest.java
@@ -7,49 +7,49 @@ import java.util.EmptyStackException;
  */
 public class StackTest {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		CalcStack s = new CalcStack();
-		double[] retval;
-		retval = s.exec("1");
-		retval = s.exec("2");
-		retval = s.exec("+");
-		printResult(retval);
-		retval=s.exec("9");
-		retval = s.exec("*");
-		printResult(retval);
-		retval = s.exec("111");
-		retval = s.exec("222");
-		retval = s.exec("show");
-		printResult(retval);
-		retval = s.exec("*");
-		printResult(retval);
-		retval = s.exec("/");
-		printResult(retval);
-		try {
-			s.exec("print");
-		} catch (UnsupportedOperationException e){
-			System.out.println("?");
-		}
-		try {
-			s.exec("+");
-		} catch (EmptyStackException e){
-			System.out.println("not enough numbers on the stack!");
-		}
-	}
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        CalcStack s = new CalcStack();
+        double[] retval;
+        retval = s.exec("1");
+        retval = s.exec("2");
+        retval = s.exec("+");
+        printResult(retval);
+        retval=s.exec("9");
+        retval = s.exec("*");
+        printResult(retval);
+        retval = s.exec("111");
+        retval = s.exec("222");
+        retval = s.exec("show");
+        printResult(retval);
+        retval = s.exec("*");
+        printResult(retval);
+        retval = s.exec("/");
+        printResult(retval);
+        try {
+            s.exec("print");
+        } catch (UnsupportedOperationException e){
+            System.out.println("?");
+        }
+        try {
+            s.exec("+");
+        } catch (EmptyStackException e){
+            System.out.println("not enough numbers on the stack!");
+        }
+    }
 
-	public static void printResult(double [] retval){
-		if (retval == null){
-			System.out.println("null");
-		} else {
-			for(double d:retval){
-				System.out.print(d+" ");
-			}
-			System.out.println();
-		}
-	}
+    public static void printResult(double [] retval){
+        if (retval == null){
+            System.out.println("null");
+        } else {
+            for(double d:retval){
+                System.out.print(d+" ");
+            }
+            System.out.println();
+        }
+    }
 
 }
