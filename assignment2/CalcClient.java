a5ee2b2a6be33356a30517990be984c28720d7b8
Bilal Quadri <bilalquadri92@gmail.com>
2013-02-08 21:10:11 -0500

    Demo code inserted with some added javadocs

--- assignment2/CalcClient.java
+++ assignment2/CalcClient.java
@@ -1,3 +1,6 @@
+import java.io.*;
+import java.net.*;
+
 /**
  * Client for calculations
  *
@@ -5,6 +8,36 @@
  * @author Bilal Quadri
  * @author Bryant Satterfield
  */
-
 public class CalcClient {
+
+    /**
+     * Main method
+     *
+     * @throws Exception
+     * @param args Unused command line arguments
+     */
+    public static void main(String args[]) throws Exception {
+      String line;	// user input
+      String server = "localhost";	// default server
+      BufferedReader userdata = new BufferedReader(new InputStreamReader(System.in));
+
+      if (args.length > 1) {
+        System.err.println("usage: java TCPClient server_name");
+        System.exit(1);
+      } else if (args.length == 1) {
+        server = args[0];
+        System.out.println("server = " + server);
+      }
+
+      Socket sock = new Socket(server, 12345);	// connect to localhost port 12345
+      DataOutputStream toServer = new DataOutputStream(sock.getOutputStream());
+      BufferedReader fromServer = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+
+      while ((line = userdata.readLine()) != null) {
+        toServer.writeBytes(line + '\n');	// send the line to the server
+        String result = fromServer.readLine();	// read a one-line result
+        System.out.println(result);		// print it
+      }
+      sock.close();	// we're done with the connection
+    }
 }
--- assignment2/CalcServer.java
+++ assignment2/CalcServer.java
@@ -1,3 +1,6 @@
+import java.io.*;
+import java.net.*;
+
 /**
  * Server for calculations
  *
@@ -5,6 +8,57 @@
  * @author Bilal Quadri
  * @author Bryant Satterfield
  */
+public class CalcServer implements Runnable {
+    Socket conn;
 
-public class CalcServer {
+    /**
+     * Constructor
+     *
+     * @param sock The socket the CalcServer should connect on
+     */
+    public CalcServer(Socket sock) {
+        this.conn = sock;
+    }
+
+    /**
+     * Main method
+     *
+     * @throws Exception
+     * @param args Command line arguments that are not used
+     */
+    public static void main(String[] args) throws Exception {
+        ServerSocket svc = new ServerSocket(12345, 5);	// listen on port 12345
+
+        for (;;) {
+            Socket conn = svc.accept();	// get a connection from a client
+            System.out.println("got a new connection");
+
+            new Thread(new CalcServer(conn)).start();
+        }
+    }
+
+    /**
+     * Run method
+     *
+     * Runs the CalcServer
+     */
+    public void run() {
+        try {
+            BufferedReader fromClient = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            DataOutputStream toClient = new DataOutputStream(conn.getOutputStream());
+            String line;
+
+            while ((line = fromClient.readLine()) != null) {	// while there's data from the client
+                System.out.println("got line \"" + line + "\"");
+
+                String result = line.length() + ": " + line.toUpperCase() + '\n';	// do the work
+
+                toClient.writeBytes(result);	// send the result
+            }
+            System.out.println("closing the connection\n");
+            conn.close();		// close connection and exit the thread
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
 }
