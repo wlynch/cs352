10895cb8488c1a4200db4f7bbba0112b1b28dc9a
bryantcs92 <amidamaren@gmail.com>
2013-02-11 19:16:21 -0500

    slightly better usage message, arguments finished
    made usage message on error more user friendly
    works with 0, 1, or 2 command-line arguments
    coding with no sleep seems like a bad idea...

--- assignment2/CalcClient.java
+++ assignment2/CalcClient.java
@@ -16,22 +16,31 @@ public class CalcClient {
      * @throws Exception
      * @param args Unused command line arguments
      */
-    public static void main(String args[]) throws Exception {
+    public static void main(String[] args) throws Exception {
       String line;	// user input
       String server = "localhost";	// default server
+      int port = 8081;
       BufferedReader userdata = new BufferedReader(
           new InputStreamReader(System.in)
       );
 
-      if (args.length > 1) {
-        System.err.println("usage: java TCPClient server_name");
+      if (args.length > 2) {
+        System.err.println("usage:	java CalcClient [hostname [port]]");
+        System.err.println("or:		java CalcClient [hostname]");
+        System.err.println("or:		java CalcClient");
         System.exit(1);
       } else if (args.length == 1) {
         server = args[0];
         System.out.println("server = " + server);
+      } else if (args.length == 2) {
+      	server = args[0];
+      	System.out.println("server = " + server);
+      	port = args[1];
+      	System.out.println("port = " + port);
       }
+      
 
-      Socket sock = new Socket(server, 12345);	// connect to port 12345
+      Socket sock = new Socket(server, port);	// connect to port 12345
       DataOutputStream toServer = new DataOutputStream(sock.getOutputStream());
       BufferedReader fromServer = new BufferedReader(
           new InputStreamReader(sock.getInputStream())
