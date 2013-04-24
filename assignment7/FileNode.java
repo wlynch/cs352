36575cb5c73b8af5d3695bd6b33d341e5cff87c4
Bilal Quadri <bilalquadri92@gmail.com>
2013-04-24 00:30:50 -0400

    Javadocing files

--- assignment7/FileNode.java
+++ assignment7/FileNode.java
@@ -1,16 +1,35 @@
+/**
+ * FileNode
+ *
+ * @author William Lynch
+ * @author Bilal Quadri
+ * @author Bryant Satterfield
+*/
 public class FileNode {
 	private String name;
 	private byte[] data;
 
+  /**
+   * Constructor for a FileNode.
+   *
+   * @param name Name of file
+   * @param data The contents of the file as a byte array
+   */
 	public FileNode(String name, byte[] data) {
 		this.name=name;
 		this.data=data;
 	}
 
+  /**
+   * Getter for the name of a FileNode.
+   */
 	public String getName() {
 		return name;
 	}
 
+  /**
+   * Getter for the data of a FileNode.
+   */
 	public byte[] getData() {
 		return data;
 	}
--- assignment7/Hash.java
+++ assignment7/Hash.java
@@ -1,7 +1,23 @@
 import java.security.*;
 import java.io.*;
 
+/**
+ * Hash
+ * Helper class with static methods for generating MD5 hashes.
+ *
+ * @author William Lynch
+ * @author Bilal Quadri
+ * @author Bryant Satterfield
+*/
+
 public class Hash {
+
+  /**
+   * Generates and MD5 hash of a path to a file.
+   *
+   * @param pathToFile String representation of path to a file
+   * @return MD5 digest of the path
+   */
 	public static String generate(String pathToFile) {
 		try{
 			MessageDigest md = MessageDigest.getInstance("MD5");
@@ -12,7 +28,6 @@ public class Hash {
 			for (int i = 0; i < mdbytes.length; i++) {
 				sb.append(Integer.toString((mdbytes[i] & 0xff) + 0x100, 16).substring(1));
 			}
-			//System.out.println("in hex :: "+ sb.toString());
 
 			digest = sb.toString();
 			return digest;
